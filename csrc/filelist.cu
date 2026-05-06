815be5aecf5d4bff77a9ade5f9b7f8f1acfc0a9b
Jason Huynh2 <019175806@coe-ee-cad2.sjsuad.sjsu.edu>
2026-05-06 15:38:35 -0700

    e

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -4,12 +4,12 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _228471_archive_1.so
-_228471_archive_1.so : archive.0/_228471_archive_1.a
+ARCHIVE_OBJS += _233779_archive_1.so
+_233779_archive_1.so : archive.0/_233779_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -z notext -m elf_i386  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_228471_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -z notext -m elf_i386  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_233779_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_228471_archive_1.so $@
+	@ln -sf .//../simv.daidir//_233779_archive_1.so $@
 
 
 
